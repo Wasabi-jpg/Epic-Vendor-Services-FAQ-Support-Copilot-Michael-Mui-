a8943cf04ee5262511119427fe1690c90565f3f6
Michael Mui <michael.mui09@gmail.com>
2026-03-02 22:01:59 -0800

    Learning about FastAPI on the job

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Query(BaseModel):
+    query: str
 
 @app.get("/")
 def read_root():
     return {"status": "FastAPI is running!"}
 
+# @app.post("/chat")
+# def send_query(user_query: str):
+#     query_item = Query(user_query)
+#     return query_item
